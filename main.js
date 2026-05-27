1fd22a6b8c6f6d689b57a2002b44b49649cb8ef0
bjh6078 <89393869+bjh6078@users.noreply.github.com>
2026-05-27 21:19:42 +0900

    Create main.js

---
+++ main.js
@@ -0,0 +1,11 @@
+// 상대 경로를 이용해 각 폴더의 파일을 가져옵니다.
+import { playBass } from './BASS/sketch.js';
+import { playKeyboard } from './keyboard/sketch.js';
+
+function main() {
+    console.log("밴드 합주 시작!");
+    playBass();
+    playKeyboard();
+}
+
+main();
